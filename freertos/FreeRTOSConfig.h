d7a346c1fcac8811c99a0a7faa62eaca755d0965
nullsub <chrisudeussen@gmail.com>
2011-04-09 23:35:29 +0200

    changed heap size. otherwise tasks fail silently

--- freertos/FreeRTOSConfig.h
+++ freertos/FreeRTOSConfig.h
@@ -19,17 +19,17 @@
 #define configUSE_IDLE_HOOK		0
 #define configUSE_TICK_HOOK		0
 #define configCPU_CLOCK_HZ		( ( unsigned long ) 24000000 )
-#define configTICK_RATE_HZ		( ( portTickType ) 1000 )
+#define configTICK_RATE_HZ		( ( portTickType ) 1000 ) //( ( portTickType ) 1000 )
 #define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 5 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 256 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 4 * 1024 ) )
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 6 * 1024 ) ) // sometimes not enough LOL
 #define configMAX_TASK_NAME_LEN		( 16 )
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
 #define configUSE_MUTEXES		1
 #define configUSE_RECURSIVE_MUTEXES	1
-#define configUSE_COUNTING_SEMAPHORES	0
+#define configUSE_COUNTING_SEMAPHORES	1
 #define configUSE_ALTERNATIVE_API	0
 #define configCHECK_FOR_STACK_OVERFLOW	0
 #define configQUEUE_REGISTRY_SIZE	0
