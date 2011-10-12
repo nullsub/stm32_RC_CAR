68d413fe060dba1c65f4b5b34e86f54d75a249ff
Chrisu de <chrisudeussen@gmail.com>
2011-10-12 14:11:47 +0200

    Revert "Revert "Revert "added some debugging commands."""
    This reverts commit 469f0141ae0865c1a8f1ae654a1933935441983f.

--- car/common/tprintf.c
+++ car/common/tprintf.c
@@ -221,10 +221,10 @@ int tsnprintf(char *out, unsigned int count, const char *format, ...)
 }
 #endif //USE_TERMINAL
 
-int data_out(char *data, int cnt)
+int data_out(char * data, unsigned int cnt)
 {
-	for (int i = 0; i < cnt; i++) {
-		putchar((char)*(data + i));
+	for (unsigned int i = 0; i < cnt; i++) {
+		putchar(*(data + i*sizeof(char)));
 	}
 	return cnt;
 }
