2bba32aefcfc07bf0b3b22e14ecb5fe134749e84
Chrisu de <chrisudeussen@gmail.com>
2011-10-12 15:44:42 +0200

    cleaned this one up. I realised that I dont even have the hardware... anyway, temp is not that important

--- car/hardware/temperature.c
+++ car/hardware/temperature.c
@@ -1,21 +1,21 @@
-#ifndef TEMPERATURE_C
-#define TEMPERATURE_C
-
 #include "temperature.h"
-#include <stdlib.h>		// itoa
-    
-/* STM32 includes */ 
+#include "common.h" //itoa
+
+/*-------defines---------*/
+#define DS1621_Write  0x90
+#define DS1621_Read   0x91
+
+
+/* STM32 includes */
 #include <stm32f10x.h>
 #include <stm32f10x_conf.h>
-void init_temperature()
-{
-	
-/*	I2C_InitTypeDef i2c_init;
 
+void init_temperature()
+{
+	I2C_InitTypeDef i2c_init;
 	I2C_StructInit(&i2c_init);
 	
-	
-	i2c_init.I2C_ClockSpeed = 
+	i2c_init.I2C_ClockSpeed =
 	i2c_init.I2C_Mode = 
 	i2c_init.I2C_DutyCycle = 
 	i2c_init.I2C_OwnAddress1 = 
@@ -23,12 +23,12 @@
 	i2c_init.I2C_AcknowledgedAddress =
 
 	I2C_Init(I2C1, &i2c_init);
-
 	I2C_Cmd();
-*/ 
-} void get_temperature(unsigned int adress, char *s)
+}
+
+void get_temperature(unsigned int adress, char *s)
 {				// adress of the i2c ds1621 sensor
-/*	unsigned char   TempH, TempL;
+	unsigned char   TempH, TempL;
 	adress &= 0x0E;
 	i2c_start(DS1621_Write|adress);
 	i2c_write(0xEE);
@@ -46,12 +46,9 @@
 	*s++ = '.';
 	if(TempL == 0x80){ 
 		*s++ = '5';
-		
 	}
 	else{
 		*s++ = '0';
 	}
 	*s = 0x00; //mark endo of string
-*/ 
-}    
-#endif	/*  */
+} 
