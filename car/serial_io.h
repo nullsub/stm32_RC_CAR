9fa5652179ce66d3e6a51d59899368df2b1bc0f5
Chrisu de <chrisudeussen@gmail.com>
2011-10-25 13:41:30 +0200

    increase Command Buffer size.
    Now all commands fit into the buffer...

--- car/serial_io.h
+++ car/serial_io.h
@@ -10,7 +10,7 @@ void serial_task(void *pvParameters) NORETURN;
 #ifdef USE_TERMINAL
 #define TERM_CMD_LENGTH		20
 #else
-#define MAX_COMMAND_LENGTH	20
+#define MAX_COMMAND_LENGTH	30
 #define DEBUG_MODE	'0'
 #define UPDATE_MODE	'1'
 #define REQUEST_MODE	'2'
